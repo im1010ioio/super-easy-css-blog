32d302175757ae743157c69ba6d11fed83486488
陳依琳 <im1010ioio@gmail.com>
2026-01-08 20:18:00 +0800

    update social info

--- .astro/content-modules.mjs
+++ .astro/content-modules.mjs
@@ -1,10 +1,10 @@
 
 export default new Map([
 ["src/content/docs/index.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdocs%2Findex.mdx&astroContentModuleFlag=true")],
-["src/content/docs/fr/index.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdocs%2Ffr%2Findex.mdx&astroContentModuleFlag=true")],
-["src/content/docs/reference/configuration.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdocs%2Freference%2Fconfiguration.mdx&astroContentModuleFlag=true")],
-["src/content/docs/getting-started/navigation.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdocs%2Fgetting-started%2Fnavigation.mdx&astroContentModuleFlag=true")],
 ["src/content/docs/reference/plugin.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdocs%2Freference%2Fplugin.mdx&astroContentModuleFlag=true")],
+["src/content/docs/reference/configuration.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdocs%2Freference%2Fconfiguration.mdx&astroContentModuleFlag=true")],
+["src/content/docs/fr/index.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdocs%2Ffr%2Findex.mdx&astroContentModuleFlag=true")],
+["src/content/docs/getting-started/navigation.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdocs%2Fgetting-started%2Fnavigation.mdx&astroContentModuleFlag=true")],
 ["src/content/docs/fr/reference/configuration.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdocs%2Ffr%2Freference%2Fconfiguration.mdx&astroContentModuleFlag=true")],
 ["src/content/docs/fr/reference/plugin.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdocs%2Ffr%2Freference%2Fplugin.mdx&astroContentModuleFlag=true")],
 ["src/content/docs/getting-started/introduction/overview.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdocs%2Fgetting-started%2Fintroduction%2Foverview.mdx&astroContentModuleFlag=true")],
