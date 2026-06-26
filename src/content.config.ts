40aacbfa1686548dd05a942c08158985102b3af3
陳依琳 <im1010ioio@gmail.com>
2026-06-26 13:56:12 +0800

    delete CTA section

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,30 +1,6 @@
-import { defineCollection, z } from "astro:content";
+import { defineCollection } from "astro:content";
 import { docsLoader, i18nLoader } from "@astrojs/starlight/loaders";
 import { docsSchema, i18nSchema } from "@astrojs/starlight/schema";
-import { glob } from "astro/loaders";
-
-
-const ctaSection = defineCollection({
-  loader: glob({
-    pattern: "**/*.{md,mdx}",
-    base: "src/content/sections",
-  }),
-  schema: z.object({
-    title: z.string().optional(),
-    description: z.string().optional(),
-    enable: z.boolean().optional(),
-    fill_button: z.object({
-      label: z.string().optional(),
-      link: z.string().optional(),
-      enable: z.boolean().optional(),
-    }),
-    outline_button: z.object({
-      label: z.string().optional(),
-      link: z.string().optional(),
-      enable: z.boolean().optional(),
-    }),
-  }),
-});
 
 export const collections = {
   docs: defineCollection({
@@ -32,5 +8,4 @@ export const collections = {
     schema: docsSchema(),
   }),
   i18n: defineCollection({ loader: i18nLoader(), schema: i18nSchema() }),
-  ctaSection,
 };
